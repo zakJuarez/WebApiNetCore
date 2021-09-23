1cf6457d8754cff0cb307862441d0ee1614ddcba
Isaac Juarez <ijuarez@fujifilm.com.mx>
2021-09-23 10:51:12 -0500

    Variables de ruta

--- backend/Controllers/AutoresController.cs
+++ backend/Controllers/AutoresController.cs
@@ -20,13 +20,55 @@ namespace backend.Controllers
         }
 
         [HttpGet]
+        [HttpGet("listado")]
         public async Task<ActionResult<List<Autor>>> get()
         {
-            return await context.Autores.ToListAsync();
+            return await context.Autores.Include(l => l.Libros).ToListAsync();
             //return new List<Autor>() {
             //    new Autor(){ Id = 1,Nombre = "Isaac Juarez" },
             //    new Autor(){ Id = 2, Nombre = "Juarez" }
             //};
+        }
+
+        [HttpGet("primero")]
+        public async Task<ActionResult<Autor>> PrimerAutor()
+        {
+            return await context.Autores.FirstOrDefaultAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Autor>> Get(int id)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(c => c.Id == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return autor;
+        }
+
+        //[HttpGet("{id:int}/{param2}")] //=> api/autores/1/otroparametro
+        //[HttpGet("{id:int}/{param2?}")] //=> api/autores/1/    ---> otroparametro puede ser opcional
+        //[HttpGet("{id:int}/{param2=persona}")] //=> api/autores/1/    ---> otroparametro toma el valor de 'persona' en caso de ser nulo, o si lleva el otro parametro toma ese valor
+        //public async Task<ActionResult<Autor>> Get(int id)
+        //{
+        //    var autor = await context.Autores.FirstOrDefaultAsync(c => c.Id == id);
+        //    if(autor == null)
+        //    {
+        //        return NotFound(); 
+        //    }
+        //    return autor;
+        //}
+
+        [HttpGet("{nombre}")]
+        public async Task<ActionResult<Autor>> Get(string nombre)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(c => c.Nombre.ToLower().Contains(nombre.ToLower()));
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return autor;
         }
 
         [HttpPost]
