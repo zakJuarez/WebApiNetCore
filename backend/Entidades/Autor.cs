468bbbe2d89e2c5b6a377165a8ad4985efbde4b5
Isaac Juarez <ijuarez@fujifilm.com.mx>
2021-09-23 13:11:05 -0500

    Validaciones por defecto

--- backend/Entidades/Autor.cs
+++ backend/Entidades/Autor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +10,18 @@ namespace backend.Entidades
     public class Autor
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="El campo {0} es requerido para la creación de un autor.")]
+        [StringLength(maximumLength:5, ErrorMessage ="El campo {0} no debe tener una longitud mayor a {1} carácteres.")]
         public string Nombre { get; set; }
+        [Range (10,100)]
+        [NotMapped]
+        public int Edad { get; set; }
+        [CreditCard]
+        [NotMapped]
+        public string TarjetaDeCredito{ get; set; }
+        [Url]
+        [NotMapped]
+        public string ip { get; set; }
         public List<Libro> Libros { get; set; }
     }
 }
